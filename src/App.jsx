66e9066bd6167fe0069eb2da1a75ad3f67c2524e
mrinnnmoy <mrinmoy202000@gmail.com>
2025-03-07 15:30:10 +0530

    build-home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./App.css";
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <div>Portfolio</div>
+    <>
+      <Home />
+    </>
   )
 };
 
