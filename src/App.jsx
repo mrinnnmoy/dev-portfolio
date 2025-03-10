6619952c27e08b7e4e4f58a103cad630f0d5815b
mrinnnmoy <mrinmoy202000@gmail.com>
2025-03-10 11:42:44 +0530

    build skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./App.css";
 import Home from './components/Home';
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 const App = () => {
   return (
     <>
       <Home />
       <Projects />
+      <Skills />
     </>
   )
 };
