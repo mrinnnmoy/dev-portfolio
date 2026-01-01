a06647bcbaa1c392b0846d106c35391dc40fcf96
mrinnnmoy <mrinmoy202000@gmail.com>
2026-01-01 19:59:59 +0530

    Setup-Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter, Routes, Route } from "react-router";
+import BlogsPage from "./pages/BlogsPage.jsx";
+import ExperiencePage from "./pages/ExperiencePage.jsx";
+import ProjectsPage from "./pages/ProjectsPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Blogs" element={<BlogsPage />} />
+      <Route path="/Experience" element={<ExperiencePage />} />
+      <Route path="/Projects" element={<ProjectsPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>,
+);
